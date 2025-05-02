515e0c06b9874b41de0872fe4994ea4292567bac
RomanPeterP <roman@aspnetprofi.com>
2025-05-02 10:20:02 +0200

    upd

---
+++ TRSAP08/TableReservationSystem/Viewmodels/RestaurantFormViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TableReservationSystem.Viewmodels
+{
+    public class RestaurantFormViewModel
+    {
+        public int RestaurantId { get; set; }  // Bei "Create" kann man ihn weglassen
+
+        [Required(ErrorMessage = "Der Name ist erforderlich.")]
+        [MaxLength(30)]
+        [Display(Name = "Name des Restaurants")]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "Die PLZ ist erforderlich.")]
+        [MaxLength(15)]
+        [Display(Name = "Postleitzahl")]
+        public string PostalCode { get; set; } = null!;
+
+        [Required(ErrorMessage = "Der Ort ist erforderlich.")]
+        [MaxLength(20)]
+        [Display(Name = "Ort")]
+        public string City { get; set; } = null!;
+
+        [Required(ErrorMessage = "Straße und Hausnummer sind erforderlich.")]
+        [MaxLength(25)]
+        [Display(Name = "Straße und Hausnummer")]
+        public string StreetHouseNr { get; set; } = null!;
+
+        [Display(Name = "Aktiv")]
+        public bool Activ { get; set; }
+
+        [Required(ErrorMessage = "Das Land ist erforderlich.")]
+        [MaxLength(2)]
+        [Display(Name = "Land")]
+        public string Country { get; set; } = null!;
+
+        // Aufgelöste Kontaktinformationen
+        [Phone(ErrorMessage = "Bitte eine gültige Telefonnummer eingeben.")]
+        [MaxLength(20)]
+        [Display(Name = "Telefonnummer")]
+        public string? PhoneNumber { get; set; }
+
+        [EmailAddress(ErrorMessage = "Bitte eine gültige E-Mail-Adresse eingeben.")]
+        [MaxLength(50)]
+        [Display(Name = "E-Mail-Adresse")]
+        public string? Email { get; set; }
+        public string? Message { get; set; }
+    }
+}
