fc5b3debe5880ead12616538f904ab039dc2f1b7
RomanPeterP <roman@aspnetprofi.com>
2025-07-08 15:58:37 +0200

    upd

---
+++ TRSAP09V2/TRSAP09V2Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TRSAP09V2Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
