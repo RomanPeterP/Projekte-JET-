b118675a293707cd8fc3053646607f2480abe9e5
RomanPeterP <roman@aspnetprofi.com>
2025-07-18 12:03:42 +0200

    upd

--- TRSAP08/TableReservationSystem/Viewmodels/RestaurantViewModel.cs
+++ TRSAP08/TableReservationSystem/Viewmodels/RestaurantViewModel.cs
@@ -5,9 +5,9 @@ namespace TableReservationSystem.Viewmodels
 {
     public class RestaurantViewModel
     {
-      
+
         public int RestaurantId { get; set; }
-        
+
         public string Name { get; set; } = null!;
         [DisplayName("Adresse")]
         public string AddressSummary { get; set; } = null!;
@@ -15,5 +15,18 @@ namespace TableReservationSystem.Viewmodels
         public string? Email { get; set; }
         [DisplayName("Telefonnummer")]
         public string? PhoneNumber { get; set; }
+        public List<string>? DocsTags
+        {
+            get
+            {
+                return new List<string>
+            {
+                "<img src=\"https://www.rahmenversand.de/media/magefan_blog/2020/10/restaurant-einrichten.jpg.webp\" />" ,
+                 "<img src=\"https://www.rahmenversand.de/media/magefan_blog/2020/10/restaurant-einrichten.jpg.webp\" />" ,
+                  "<img src=\"https://www.rahmenversand.de/media/magefan_blog/2020/10/restaurant-einrichten.jpg.webp\" />"
+
+            };
+            }
+        }
     }
 }
