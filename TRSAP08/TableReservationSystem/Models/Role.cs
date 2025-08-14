7d3697ca7970d8827c5c0356ddcab74b69302e47
Robert Liebhart <snek@live.at>
2025-08-14 13:46:10 +0200

    register-login-addedModels

---
+++ TRSAP08/TableReservationSystem/Models/Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TableReservationSystem.Models.Interfaces;
+
+namespace TableReservationSystem.Models
+{
+    public class Role : IRole
+    {
+        public int RoleId { get; set; }
+
+        public string RoleName { get; set; } = null!; 
+    }
+}
---
+++ TRSAP08/TableReservationSystem/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TableReservationSystem.Models.Interfaces;
+
+namespace TableReservationSystem.Models
+{
+    public class User : IUser
+    { 
+        public int UserId { get; set; }
+        public string UserName { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+        public string RoleId { get; set; } = null!;
+        public string Email { get; set; } = null!;
+    }
+}
