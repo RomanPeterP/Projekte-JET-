5e0b8aa663c4a2892a2ba9abb4a654b92feff3fe
Roman <122753048+Moavalon@users.noreply.github.com>
2025-08-14 13:34:22 +0200

    added Interfaces IRole and IUser

---
+++ TRSAP08/TableReservationSystem/Models/Interfaces/IUser.cs
@@ -0,0 +1,11 @@
+﻿namespace TableReservationSystem.Models.Interfaces
+{
+    public interface IUser
+    {
+        int UserId { get; set; }
+        string UserName { get; set; }
+        string PasswordHash { get; set; }
+        string RoleId { get; set; }
+        string Email { get; set; }
+    }
+}
